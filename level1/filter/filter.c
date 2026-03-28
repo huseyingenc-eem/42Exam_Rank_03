6a356f15b1ee568581185fd41ea0d802650b09d8
huseyingenc-eem <huseyingenc.eem@gmail.com>
2026-03-28 18:52:06 +0300

    feat: add new implementations for filter, n_queens, permutations, powerset, and rip problems

--- level1/filter/filter.c
+++ level1/filter/filter.c
@@ -1,56 +1,43 @@
-//Question graded by Moulinette: 100
-
-#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
+#define BUFFER_SIZE 10
 #endif
 
-void	ft_filter(char *line, char *str)
-{
-	int	len = strlen(str);
-	int	i = 0;
-	while (line[i])
-	{
-		int	h = 0;
-		while (str[h] && line[i + h] == str[h])
-			h++;
-		if (h == len)
-		{
-			int	l = 0;
-			while (l < len)
-			{
-				write (1, "*", 1);
-				l++;
-			}
-			i += h;
-		}
-		else
-		{
-			write (1, &line[i], 1);
-			i++;
-		}
-	}
+void ft_filter(char *line, char *s) {
+  int len = strlen(s);
+  int i = 0;
+  int j;
+
+  while (line[i]) {
+    j = 0;
+    while (s[j] && line[i + j] == s[j])
+      j++;
+    if (j == len) {
+      while (j--)
+        write(1, "*", 1);
+      i += len;
+    } else
+      write(1, &line[i++], 1);
+  }
 }
 
-int	main(int argc, char **argv)
-{
-	char	line[999999];
-	int		ret = 1;
-	int		i = 0;
-	if (argc != 2 || !argv[1][0])
-		return (1);
-	while (ret > 0)
-	{
-		ret = read(0, &line[i], BUFFER_SIZE);
-		if (ret < 0)
-		{
-			perror("Error");
-			return (1);
-		}
-		i += ret;
-	}
-	ft_filter(line, argv[1]);
-	return (0);
+int main(int argc, char **argv) {
+  char line[999999];
+  int total = 0;
+  int ret;
+
+  if (argc != 2 || !argv[1][0])
+    return (1);
+  while ((ret = read(0, &line[total], BUFFER_SIZE)) > 0)
+    total += ret;
+  if (ret < 0) {
+    perror("Error");
+    return (1);
+  }
+  line[total] = '\0';
+  ft_filter(line, argv[1]);
+  return (0);
 }

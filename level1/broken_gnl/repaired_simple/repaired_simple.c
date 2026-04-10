8e1299894cb57cd2177305122e6d0a06f7a5d6a1
Hüseyin Genç <huseyingenc.eem@gmail.com>
2026-04-10 11:37:53 +0300

    refactor(get_next_line): improve code formatting and consistency

--- level1/broken_gnl/repaired_simple/repaired_simple.c
+++ level1/broken_gnl/repaired_simple/repaired_simple.c
@@ -1,18 +1,17 @@
 
 #include "repaired_simple.h"
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	static char		buf[BUFFER_SIZE];
-	static ssize_t	bytes;
-	static int		i;
-	char			*line;
-	int				j;
-
-	line = malloc(10000);
+	static char buf[999999];
+	static ssize_t bytes = 0;
+	static ssize_t i = 0;
+	ssize_t j = 0;
+	
+	char *line = malloc(10000);
 	if (!line || fd < 0)
-		return (NULL);
-	j = 0;
+		return NULL;
+
 	while (1)
 	{
 		if (i >= bytes)
@@ -20,16 +19,16 @@ char	*get_next_line(int fd)
 			i = 0;
 			bytes = read(fd, buf, BUFFER_SIZE);
 			if (bytes <= 0)
-				break ;
+				break;
 		}
 		line[j] = buf[i++];
 		if (line[j++] == '\n')
-			break ;
+			break;
 	}
 	if (!j)
 		return (free(line), NULL);
-	line[j] = 0;
-	return (line);
+	line[j] = '\0';
+	return line;
 }
 /*
 int	main(void)
